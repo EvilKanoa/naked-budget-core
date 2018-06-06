85eb0f475bb62b06e450e74c71c95336c247c21c
EvilKanoa <kanoa@kanoa.ca>
2018-06-06 10:20:40 -0400

    Added redux and folder structure

---
+++ client/src/components/App/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <center>
+          <p> NakedBudget - The budgeting app that exposes your money to reality </p>
+          <h3> Under Construction... </h3>
+        </center>
+      </div>
+    );
+  }
+}
+
+export default App;
