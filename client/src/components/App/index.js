2ef45bd992ad18814404cf4d687468a3a615a663
EvilKanoa <kanoa@kanoa.ca>
2018-06-08 21:17:01 -0400

    Removed foundation deps

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { Callout, Colors } from 'react-foundation';
 import './index.css';
 
 const App = props => {
   return (
     <div className="app">
       <center>
-        <Callout color={Colors.WARNING}>
+        <div>
           <p> NakedBudget - The budgeting app that exposes your money to reality </p>
           <h3> Under Construction... </h3>
-        </Callout>
+        </div>
       </center>
     </div>
   );
