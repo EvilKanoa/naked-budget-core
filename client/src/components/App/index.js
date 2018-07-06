7926bca5fc4175e414cf1646ec6115e2a6ef431f
EvilKanoa <kanoa@kanoa.ca>
2018-07-05 22:01:01 -0400

    Fixed vertical height bug

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -2,29 +2,26 @@ import React from 'react';
 import TopBar from '../TopBar';
 import Sidebar from '../Sidebar';
 import AppContainer from '../AppContainer';
-import { Grid, Row, Col } from 'react-bootstrap';
 import './index.css';
 
 const LOGO_IMAGE = 'logo.png';
 
 const App = () => {
     return (
-        <div id='app'>
-            <Grid fluid={true}>
-                <Row className='show-grid'>
-                    <Col xs={12} className='remove-col-padding'>
-                        <TopBar img={LOGO_IMAGE}/>
-                    </Col>
-                </Row>
-                <Row className='show-grid full-height'>
-                    <Col md={1} className='remove-col-padding full-height sidebar-width'>
-                        <Sidebar/>
-                    </Col>
-                    <Col className='remove-col-padding full-height'>
-                        <AppContainer/>
-                    </Col>
-                </Row>
-            </Grid>
+        <div className='flex-vertical' id='app'>
+            <div className='show-grid flex-fixed-child'>
+                <div className='remove-col-padding'>
+                    <TopBar img={LOGO_IMAGE}/>
+                </div>
+            </div>
+            <div className='show-grid flex-fill-child flex-horizontal'>
+                <div className='remove-col-padding sidebar-width flex-fixed-child'>
+                    <Sidebar/>
+                </div>
+                <div className='remove-col-padding flex-fill-child'>
+                    <AppContainer/>
+                </div>
+            </div>
         </div>
     );
 };
