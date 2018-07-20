dc72262cca3240ba11ea9cb3f2972dc20d116c98
EvilKanoa <kanoa@kanoa.ca>
2018-07-20 16:27:38 -0400

    Added passport to package.json

--- server/api/routes.mjs
+++ server/api/routes.mjs
@@ -1,39 +1,35 @@
 import express from 'express';
 
-const prefix = (route) => `/v1${route}`;
-
 export default () => {
     const app = express();
 
-    app.get(prefix('/teapot'), (req, res) => res.status(418).send(`I'm a teapot`));
     app.get('/teapot', (req, res) => res.status(418).send(`I'm a teapot`));
-    app.get('/', (req, res) => res.json({currentApi: prefix('/')}));
 
     // setup personal data routes
-    app.get(prefix('/personal/:id'), (req, res) => {
+    app.get('/v1/personal/:id', (req, res) => {
 
     });
-    app.post(prefix('/personal'), (req, res) => {
+    app.post('/v1/personal', (req, res) => {
 
     });
-    app.put(prefix('/personal/:id'), (req, res) => {
+    app.put('/v1/personal/:id', (req, res) => {
 
     });
-    app.delete(prefix('/personal/:id'), (req, res) => {
+    app.delete('/v1/personal/:id', (req, res) => {
 
     });
 
     // setup user data routes
-    app.get(prefix('/user/:id'), (req, res) => {
+    app.get('/v1/user/:id', (req, res) => {
 
     });
-    app.post(prefix('/user'), (req, res) => {
+    app.post('/v1/user', (req, res) => {
 
     });
-    app.put(prefix('/user/:id'), (req, res) => {
+    app.put('/v1/user/:id', (req, res) => {
 
     });
-    app.delete(prefix('/user/:id'), (req, res) => {
+    app.delete('/v1/user/:id', (req, res) => {
 
     });
 
